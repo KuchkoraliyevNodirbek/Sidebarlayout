1936b0cfecbf4c2f55669143ba687398288c765c
nod1rckk <nodirqochqaraliyev@gmail.com>
2024-02-16 12:52:59 +0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.js';
+import About from './pages/About.js';
+import Analytics from './pages/Analytics.js';
+import Comment from './pages/Comment.js';
+import Product from './pages/Product.js';
+import ProductList from './pages/ProductList.js';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Analytics.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Analytics extends Component {
+  render() {
+    return (
+      <div>Analytics</div>
+    )
+  }
+}
+
+export default Analytics
